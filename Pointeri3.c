546ec9170c0c5ce64b2a208f2f6588861ed7c338
Vlad Gheorghe <61756348+vladjacogheorghe@users.noreply.github.com>
2020-03-07 14:06:57 +0200

    Create Pointeri3.c

---
+++ Pointeri3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define tip char
+
+tip *vPtr, *vPtr2;
+
+int main()
+{
+	vPtr = (tip *)malloc(sizeof(tip));
+	vPtr2 = (tip *)malloc(sizeof(tip));
+
+	printf("%p\n", vPtr);
+	printf("%p\n", vPtr2);
+	printf("%p\n", vPtr2 + 1);
+	printf("%d\n", (vPtr2 - vPtr));
+
+	system("pause");
+	return 0;
+}
