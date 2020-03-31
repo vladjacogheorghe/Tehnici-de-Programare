132b1f11797437b1217ca466145c9b02a5899dd6
Vlad Gheorghe <61756348+vladjacogheorghe@users.noreply.github.com>
2020-03-31 17:38:25 +0300

    Update P3.c

--- L5/P3.c
+++ L5/P3.c
@@ -10,47 +10,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_SIZE 100
-int st[50], vf = -1;
+
+int palindrom(int n)
+{
+	int aux, inv = 0;
+	aux = n;
+	while (n != 0)
+	{
+		inv = inv * 10 + n % 10;
+		n = n / 10;
+	}
+	if (aux == inv)
+	{
+		return 1; //este palindrom
+	}
+	else
+	{
+		return 0; //nu e palindrom
+	}
+}
 void push(int x, int *vf, int st[])
 {
 	st[++(*vf)] = x;
 }
-void pop(int *vf, int st[])
-{
-	*vf = -1;
-}
-int peek(int *vf, int st[])
-{
-	return st[*vf];
-}
 int main()
 {
-	int aux, ok, n, nr;
+	int st[50], vf = -1;
+	int i, n, nr;
 	printf("Numere citite: "); scanf("%d", &n);
 	for (int i = 0; i < n; i++)
 	{
-		printf("nr = "); scanf("%d", &nr);
-		aux = nr; vf = -1; ok = 0;
-		while (aux)
+		printf("Dati un numar:"); scanf("%d", &nr);
+		if (palindrom(nr) == 1)
 		{
-			push(aux % 10, &vf, st);
-			aux /= 10;
+			push(nr, &vf, st);
 		}
-		aux = nr;
-		while (aux)
-		{
-			if (aux % 10 != peek(&vf, st))
-			{
-				ok = 1;
-				break;
-			}
-			aux /= 10;
-			vf--;
-		}
-		if (ok == 0)
-			printf("Numarul este palindrom\n");
-		else
-			printf("Numarul nu este palindrom\n");
+	}
+	for (i = 0; i <= vf; i++)
+	{
+		printf("%d\n", st[i]);
 	}
 	system("pause");
 	return 0;
